42ecfacf17cfd0922779a26f54e7b0957f39cb81
Kves <vnlkbn@gmail.com>
2025-03-15 16:15:01 +0100

    quicksort

---
+++ sum.py
@@ -0,0 +1,11 @@
+def sum(list):
+    if list == []:
+        return 0
+    return list[0] + sum(list[1:])
+
+def count(list):
+    if list == []:
+        return 0
+    return 1 + count(list[1:])
+
+print(sum([1,2,3,4,5]))
